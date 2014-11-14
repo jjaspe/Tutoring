04c6cd4b8a32b436a3e4e1d3c4b8a29a03bd437c
jjaspe <jjaspe09@hotmail.com>
2014-11-13 22:31:37 -0500

    Updated main.css and index.html

--- tutoring.js
+++ tutoring.js
@@ -1,12 +1,28 @@
-var main=function(){
-	$('.next-arrow').click(function(){
-		var currentSlide=$('.active-slide'),
-		    nextSlide=$('.active-slide').next();
-		
-		currentSlide.fadeOut(600).removeClass('active-slide');
-		nextSlide.fadeIn(600).addClass('active-slide');
-	});
-}
+
+main = function () {
+    $('.arrow-next').click(function () {
+        var currentSlide = $('.active-slide'),
+            nextSlide = $('.active-slide').next();
+
+        if (nextSlide.length == 0)
+            nextSlide = $('.slide').first();
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+    });
+
+    $('.arrow-prev').click(function () {
+        var currentSlide = $('.active-slide'),
+            nextSlide = $('.active-slide').prev();
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+
+        if (nextSlide.length == 0)
+            nextSlide = $('.slide').last();
+
+        nextSlide.fadeIn(600).addClass('active-slide');
+    });
+};
 	
 $(document).ready(main);
 	
